ae651b58da386b4a0d8c80437031011f81f37d6e
Blooming Burden <flowersoverskulls96@gmail.com>
2023-06-27 12:35:57 +0300

    Scrolling

--- src/js/modules/smoothScroll.js
+++ src/js/modules/smoothScroll.js
@@ -1,20 +1,54 @@
-window.addEventListener('wheel', function(event) {
-    // Получаем текущую позицию скролла
-    var currentScrollPosition = window.pageYOffset;
+class Scrooth {
+    constructor({element = window, strength=13, acceleration = 1.2,deceleration = 0.975}={}) {
+      this.element = element;
+      this.distance = strength;
+      this.acceleration = acceleration;
+      this.deceleration = deceleration;
+      this.running = false;
+      this.maxScroll = Math.max( document.body.scrollHeight, document.body.offsetHeight, 
+        document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight );
   
-    // Определяем направление скролла
-    var direction = event.deltaY > 0 ? 1 : -1;
+      this.element.addEventListener('wheel', this.scrollHandler.bind(this), {passive: false});
+      this.element.addEventListener('mousewheel', this.scrollHandler.bind(this), {passive: false});
+      this.scroll = this.scroll.bind(this);
+    }
   
-    // Вычисляем следующую позицию скролла
-    var nextScrollPosition = currentScrollPosition + direction * 170;
-    
+    scrollHandler(e) {
+      e.preventDefault();
+  
+      if (!this.running) {
+        this.top = this.element.pageYOffset || this.element.scrollTop || 0;
+        this.running = true;
+        this.currentDistance = e.deltaY > 0 ? 0.1 : -0.1;
+        this.isDistanceAsc = true;
+        this.scroll();
+      } else {
+        this.isDistanceAsc = false;
+        this.currentDistance = e.deltaY > 0 ? this.distance : -this.distance;
+      }
+    }
+  
+    scroll() {
 
-    // Изменяем позицию скролла с плавной анимацией
-    window.scrollTo({
-      top: nextScrollPosition,
-      behavior: 'smooth'
-    });
+      if (this.running) {
+        this.currentDistance *= this.isDistanceAsc === true ? this.acceleration : this.deceleration;
+        Math.abs(this.currentDistance) < 0.1 && this.isDistanceAsc === false ? this.running = false : 1;
+        Math.abs(this.currentDistance) >= Math.abs(this.distance) ? this.isDistanceAsc = false : 1;
+        
+        if (this.top + window.innerHeight > this.maxScroll) {
+            this.top = this.maxScroll - window.innerHeight;
+        } else if (this.top < 0) {
+            this.top = 0;
+        } else {
+            this.top += this.currentDistance;
+        }
+
+        this.element.scrollTo(0, this.top);
+        
+        requestAnimationFrame(this.scroll);
+      }
+    }
+  }
   
-    // Отменяем действие по умолчанию
-    event.preventDefault();
-  });
+  const body = new Scrooth();
+  
